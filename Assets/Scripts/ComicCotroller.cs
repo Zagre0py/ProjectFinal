cbd91bce7128e967003efc95c7726cbe10170a27
Tokayushi <136655302+Tokayushi@users.noreply.github.com>
2025-03-31 15:59:23 -0500

    primera implementacion comic

---
+++ Assets/Scripts/ComicCotroller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComicCotroller : MonoBehaviour
+{
+    [SerializeField] GameObject[] part1;
+
+    int partCount = 1;
+   
+
+    public bool inLvl1, inLvl2, inLvl3; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (inLvl1)
+        {
+            while (part1 =!part1.Length()) 
+            {
+                part1[1].SetActive(true);
+
+            }
+
+        }*/
+    }
+}
